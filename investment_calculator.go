3b2efaf61b36973636389e8acc26908966a421de
Nalin Ekanayake <nalin@hsenidoutsourcing.com>
2024-04-13 20:07:15 +0530

    add fmt scan function

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 
-	investmentAmount, years := 1000.0, 10.0
+	const inflationRate = 2.5
+	var investmentAmount float64
+	years := 10.0
 	expectedReturnRate := 5.5
 
+	fmt.Scan(&investmentAmount)
+
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
